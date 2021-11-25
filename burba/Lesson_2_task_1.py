c6cb76691559f360a7cb7ef39a94e1588095f51d
Mozgolom <voytima@gmail.com>
2021-11-25 23:37:43 +0300

    Lesson_2_tasks_1_2_4_5

--- burba/Lesson_2_task_1.py
+++
@@ -1,8 +0,0 @@
-#Var_1 (в строку)
-numbers = (15 * 3, 15 / 3, 15 // 2, 15 ** 2)
-for i in numbers:
-    print(type(i))
-print('*' * 70)
-#Var_2 (в строку)
-numbers = (15 * 3, 15 / 3, 15 // 2, 15 ** 2)
-print(list(map(type, numbers)))
